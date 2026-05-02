f79e2609d596c6fadced08fc02ee2e14361f63fe
pyroscorcher <pyroscorcherthariq@apps.ipb.ac.id>
2026-05-02 13:55:47 +0700

    route commit

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,6 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\EnrollmentController;
 use App\Http\Controllers\AdminDashboardController;
 use App\Http\Controllers\GalleryController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
 use App\Http\Controllers\UserController;
 
 // login admin
@@ -36,30 +32,10 @@ Route::middleware(['auth:admins'])->group(function () {
 
 });
 
-// form pendaftaran
-Route::get('/daftar', [EnrollmentController::class, 'create'])->name('pendaftaran.create');
-Route::post('/daftar', [EnrollmentController::class, 'store'])->name('pendaftaran.store');
-
-// Admin dashboard routes
-Route::middleware(['auth:admins'])->group(function () {
-    Route::get('/admin', [AdminDashboardController::class, 'index'])
-        ->name('admin.dashboard');
-    Route::get('/admin/enrollment/{id}', [AdminDashboardController::class, 'show'])->name('admin.enrollment.show');
-    Route::patch('/admin/enrollment/{id}/status', [AdminDashboardController::class, 'updateStatus'])
-        ->name('admin.enrollment.update-status');
-    Route::resource('/admin/gallery', GalleryController::class);
-
-    Route::get('/admin/gallery', [GalleryController::class, 'index'])->name('gallery.index');
-    Route::get('/admin/gallery/upload', [GalleryController::class, 'create'])->name('gallery.create');
-    Route::post('/admin/gallery/upload', [GalleryController::class, 'store'])->name('gallery.store');
-    Route::delete('/admin/gallery/{id}', [GalleryController::class, 'destroy'])->name('gallery.destroy');
-
-});
-
 //user view
-Route::get('/', [App\Http\Controllers\UserController::class, 'ShowBeranda'])->name('user.home');
-Route::get('/user/info', [App\Http\Controllers\UserController::class, 'ShowInfo'])->name('user.info');
-Route::get('/user/daftar', [App\Http\Controllers\UserController::class, 'ShowDaftar'])->name('user.daftar');
-Route::get('/user/login', [App\Http\Controllers\UserController::class, 'ShowLogin'])->name('user.login');
-Route::get('/user/pengumuman', [App\Http\Controllers\UserController::class, 'ShowPengumuman'])->name('user.pengumuman');
-Route::get('/user/kontak', [App\Http\Controllers\UserController::class, 'ShowKontak'])->name('user.kontak');
+Route::get('/', [UserController::class, 'ShowBeranda'])->name('user.home');
+Route::get('/user/info', [UserController::class, 'ShowInfo'])->name('user.info');
+Route::get('/user/daftar', [UserController::class, 'ShowDaftar'])->name('user.daftar');
+Route::get('/user/login', [UserController::class, 'ShowLogin'])->name('user.login');
+Route::get('/user/pengumuman', [UserController::class, 'ShowPengumuman'])->name('user.pengumuman');
+Route::get('/user/kontak', [UserController::class, 'ShowKontak'])->name('user.kontak');
