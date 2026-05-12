e13dddb63ca463aefc7e42ec470d6d105ba284f8
pyroscorcher <pyroscorcherthariq@apps.ipb.ac.id>
2026-05-12 15:42:44 +0700

    middleware fix #2

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/daftar', [EnrollmentController::class, 'create'])->name('pendaftara
 Route::post('/daftar', [EnrollmentController::class, 'store'])->name('pendaftaran.store');
 
 // Admin dashboard routes
-Route::middleware(['auth:admins'])->group(function () {
+Route::middleware(['auth:admin'])->group(function () {
     Route::get('/admin', [AdminDashboardController::class, 'index'])
         ->name('admin.dashboard');
     Route::get('/admin/enrollment/{id}', [AdminDashboardController::class, 'show'])->name('admin.enrollment.show');
@@ -49,19 +49,13 @@ Route::middleware(['auth:admins'])->group(function () {
 
 });
 
-//user view dan user dashboard routes
+// user view dan user dashboard routes
 Route::get('/', [UserController::class, 'ShowBeranda'])->name('user.home');
 Route::get('/user/info', [UserController::class, 'ShowInfo'])->name('user.info');
 Route::get('/user/daftar', [UserController::class, 'ShowDaftar'])->name('user.daftar');
 Route::get('/user/pengumuman', [UserController::class, 'ShowPengumuman'])->name('user.pengumuman');
 Route::get('/user/kontak', [UserController::class, 'ShowKontak'])->name('user.kontak');
 Route::get('/user/news', [UserController::class, 'ShowNews'])->name('user.news');
-Route::get('/user/dashboard_user', [UserController::class, 'userDashboard'])->name('user.dashboard_user');
-Route::get('/user/data_anak', [UserController::class, 'dataAnak'])->name('user.data_anak');
-Route::get('/user/data_orangtua', [UserController::class, 'dataOrangtua'])->name('user.data_orangtua');
-Route::get('/user/data_periodik', [UserController::class, 'dataPeriodik'])->name('user.data_periodik');
-Route::get('/user/data_prestasi', [UserController::class, 'dataPrestasi'])->name('user.data_prestasi');
-Route::get('/user/upload_dokumen', [UserController::class, 'uploadDokumen'])->name('user.upload_dokumen');
 
 // user login routes
 Route::get('/user/login', [UserController::class, 'ShowLogin'])->name('user.login');
