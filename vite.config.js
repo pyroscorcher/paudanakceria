8fbb0e140aad662ec058144fae0eb5bfa455e798
pyroscorcher <pyroscorcherthariq@apps.ipb.ac.id>
2026-05-24 19:45:10 +0700

    building ran

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js'],
+            input: [
+                'resources/js/app.js', // <-- This line MUST be here
+            ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
 });
